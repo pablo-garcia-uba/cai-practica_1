405cf2f96836239ba96ee28f4a233bda7525ed60
pablo-garcia-uba <69pg35254384@campus.economicas.uba.ar>
2025-04-04 00:35:14 -0300

    New classes
    Se agreraron las clases bede, directivo y docente heradadas de Empleado

---
+++ cai_01/Bedel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cai_01
+{
+    internal class Bedel : Empleado
+    {
+
+        public String _apodo;
+
+        public String Apodo { get => _apodo; set => _apodo = value; }
+
+        protected override void GetCredencial()
+        {
+
+        }
+
+        protected override String ListarEmpleados(bool listarConId)
+        {
+            return "Lista de empleados";
+        }
+    }
+}
